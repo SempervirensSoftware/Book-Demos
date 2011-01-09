aab47eef0815a6220821e0fa1a1d0d8dfa032fec
pterrill <peter.terrill@gmail.com>
2011-01-08 17:43:28 -0800

    initial checkin of first book demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookFirstApp::Application.config.secret_token = 'ae3af3d68764253070b198c375b06805f50ce65219fee6cf6ac1bd132469b271003ec734a4666e7c804621c8d938edb035c0f4884b86266a25152b62eafecc4c'
